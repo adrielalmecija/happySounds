c99144dd6a3f8bc0dd1fe91ffd7b14a8753f506d
adrielalmecija <54188198+adrielalmecija@users.noreply.github.com>
2024-06-30 00:29:31 -0300

    Update script.js

--- src/script.js
+++ src/script.js
@@ -16,7 +16,7 @@ class BuildScene {
 
         // Camera setup
         const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-        camera.position.z = 1; // Set camera position
+        camera.position.z = 2; // Set camera position
 
         // Scene setup
         const scene = new THREE.Scene(); // Create a new scene
